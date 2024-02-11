cc3f4b63ff427f7c9a025ef372b314adad582884
S374124 <S374124@students.cdu.edu.au>
2024-02-11 15:10:38 +0930

    status label added

--- main.py
+++ main.py
@@ -27,12 +27,14 @@ class Main:
     button3 = Button(topFrame, text="Please Close", background="red", fg="white", font = fnt.Font(size = 10, weight="bold"), width=12, height=2)
     textBox1 = Text(bottomFrame, width=100, height=10)
     labelStatus = Label(bottomFrame, text="Initiating...", font=fnt.Font(size=10))
+    labelLog = Label(bottomFrame, text="", font=fnt.Font(size=10))
     label1.grid(row=0, pady=(5, 10))
     button1.grid(row=2, column=0)
     button2.grid(row=2, column=2, padx=(10, 10))
     button3.grid(row=2, column=4)
     textBox1.grid(row=3, pady=(10, 0))
     labelStatus.grid(row=4, column=0, padx=(10, 0))
+    labelLog.grid(row=5, column=0, padx=(10, 0))
                 
     def __init__(self):  
         self.root.after(2000, self.begin_listening)
@@ -42,11 +44,15 @@ class Main:
         
     def begin_listening(self):
         voiceCommandHandler = sc.SpeechCommand(); #create class object
-        self.labelStatus.config(text="What do you want to do? I'm listening...")
+        if not self.isCommanding:
+            self.labelStatus.config(text="What do you want to do? I'm listening...")
+        self.labelLog.config(text="Speak")
+        self.root.update()
         recorded_speach = voiceCommandHandler.record_speech(5) #call a method from object
         vCommand = voiceCommandHandler.recognize_speech(recorded_speach) #call a method from object
-        if not self.isCommanding and vCommand is None:
-            self.labelStatus.config(text="What do you want to do? I'm listening...")
+        if not self.isCommanding and vCommand is None:            
+            self.labelLog.config(text="Sorry, Voice command could not be fount!")
+            self.root.update()
         elif not self.isCommanding and vCommand == "please close":
                 self.root.destroy()
         else:
@@ -56,17 +62,21 @@ class Main:
             else:
                 if vCommand is not None:
                     self.com.setTextBoxText(vCommand)
-
+                                    
             if self.com is not None:
                 self.textBox1.focus()
                 self.labelStatus.config(text=self.com.getStatusLabelText())
                 tbox = self.com.getTextBoxText()
                 if tbox == "-exit":
                     self.isCommanding = False
+                    self.labelStatus.config(text="What do you want to do? I'm listening...")
                 else:
+                    self.labelLog.config(text="Your Command: " + tbox)
                     self.textBox1.delete("1.0", "end")
                     self.textBox1.insert(END, tbox)
                     self.isCommanding = self.com.isContinue()
+            else:
+                self.labelLog.config(text="Sorry, Voice command is not matching!")
         self.root.after(2000, self.begin_listening)
 
 mn = Main()
